f3ef6e8611213b5ca12385954eec03e2b115dc7d
Floris75 <floris.decouflet@gmail.com>
2021-05-03 17:23:19 +0200

    mise en place architecture version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './Assets/App.css';
+import Header from "./Pages/Partials/Header";
+import Footer from "./Pages/Partials/Footer";
+import Home from "./Pages/Home";
+import HouseDetails from "./Pages/HouseDetails";
+import Search from './Pages/Search';
+import Forms from './Pages/Forms';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/housedetails/:id' render={(props)=> <HouseDetails {...props} />} />
+        <Route path='/signin' component={Forms}/>
+        <Route path='/signup/host' component={Forms}/>
+        <Route path='/signup/guest' component={Forms}/>
+        <Route path='/search/:city' render={(props)=> <Search {...props} />}/>
+        <Route path='/search/:checkin/:checkout' render={(props)=> <Search {...props} />}/>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
